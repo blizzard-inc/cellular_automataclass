3e6f1f25f1f9f214e9ad355d7606bed29c985867
blizzard-inc <85935202+blizzard-inc@users.noreply.github.com>
2021-06-17 15:57:51 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,21 @@
-class bord:
+class Board:
   def __init__(self,rows:list):
-    if len(rows)==0 or any(len(rows[i])==0 for i in range(len(rows))):
+    if len(rows)==0 or any([len(rows[i])==0 for i in range(len(rows))]):
       raise TypeError
     self.rows=rows
     self.nrows=len(rows)
     self.ncols=len(rows[0])
   
-    
+  def __getitem__(self,key)
+    return self.rows[key]
+  def advance(self,rule:Rules)
+
+class Neighbourhood:
+  def __init__(self,neighbours:list):
+    if any([len(neighbours[i]!=2 or any([type(neigbours[i][j])!=int for j in range(len(neighbours[i])) ]) for i in range(len(neigbours)) )])
+    neighbours.sort()
+    self.neighbours=neighbours
+
+class Rules:
+  def __init__(self,neigbours:Neighbourhood,livecases,deathcases)
+      self.livecases,
