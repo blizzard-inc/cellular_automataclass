f93d575f021f388edb20fca7b01b4dbf81c34574
blizzard-inc <85935202+blizzard-inc@users.noreply.github.com>
2021-06-22 12:30:47 +0200

    trying stuff

--- code.py
+++ code.py
@@ -1,21 +1,33 @@
-class Board:
-  def __init__(self,rows:list):
-    if len(rows)==0 or any([len(rows[i])==0 for i in range(len(rows))]):
-      raise TypeError
-    self.rows=rows
-    self.nrows=len(rows)
-    self.ncols=len(rows[0])
-  
-  def __getitem__(self,key)
-    return self.rows[key]
-  def advance(self,rule:Rules)
+import numpy
 
-class Neighbourhood:
-  def __init__(self,neighbours:list):
-    if any([len(neighbours[i]!=2 or any([type(neigbours[i][j])!=int for j in range(len(neighbours[i])) ]) for i in range(len(neigbours)) )])
-    neighbours.sort()
-    self.neighbours=neighbours
+class cell:
+    def __init__(self,state:):
+        self.state=state
 
-class Rules:
-  def __init__(self,neigbours:Neighbourhood,livecases,deathcases)
-      self.livecases,
+class board(numpy.array):
+    def __init__(self,border,*args,**kwargs):
+        self.border=border
+        super().__init__(self,*args,**kwargs)
+        
+    def advance(self,rule: rule)
+        
+
+class rule:
+    def __init__(self,death,live,border :borderrules):
+        self.birth=birth
+        self.death=death
+        self.live=live
+class sumrule(rule):
+    def __init__(self,live,death,birth,neighbours,border):
+      #neem live=[2,3]
+      #death=[0,1,4,5,6,7,8]
+      #birth=[3]
+      super().__init__(self,deaths,lives,border)
+    
+class borderrules:
+    def __init__(self,):
+
+((0,0),(0,1),(-1,0),)
+list(1,2,2,3)
+zeros(3)
+list(0,0,0)
